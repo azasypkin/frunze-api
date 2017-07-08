cbea23a9d8654be497357afae788a25cd3b3e2db
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-07-08 11:52:04 +0200

    Add support for `ComponentPropertySchema.kind` and `ComponentPropertySchema.options`.

--- src/components/component_property_schema.rs
+++ src/components/component_property_schema.rs
@@ -1,3 +1,15 @@
+/// Describes component property value option.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ComponentPropertyValueOption {
+    /// Property value option type.
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub type_name: String,
+    /// Human-readable short value option type name.
+    pub name: String,
+    /// Human-readable value option type long description.
+    pub description: String,
+}
+
 /// Describes single component property.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ComponentPropertySchema {
@@ -11,4 +23,8 @@ pub struct ComponentPropertySchema {
     /// Default value of the property.
     #[serde(rename(serialize = "defaultValue", deserialize = "defaultValue"))]
     pub default_value: String,
+    /// Property value kind (string or options).
+    pub kind: String,
+    /// Possible value options (optional, available only for properties with `kind == options`.
+    pub options: Option<Vec<ComponentPropertyValueOption>>,
 }
