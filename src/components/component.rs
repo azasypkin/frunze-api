a90721affa553c738259f626717b619fc30dd5f7
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-07-10 12:02:59 +0200

    Add support for `ComponentSchema.triggers` and `ComponentSchema.actions`.

--- src/components/component.rs
+++ src/components/component.rs
@@ -1,5 +1,14 @@
 use std::collections::HashMap;
 
+/// Describes component trigger action.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ComponentTriggerAction {
+    /// Type of the action to trigger.
+    pub action: String,
+    /// Identifier of the component where action of `type_name` type should be executed.
+    pub component: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Component {
     /// Identifier of the project component.
@@ -9,4 +18,6 @@ pub struct Component {
     pub type_name: String,
     /// Values of project component properties.
     pub properties: HashMap<String, String>,
+    /// Component trigger <-> corresponding actions map.
+    pub triggers: HashMap<String, Vec<ComponentTriggerAction>>,
 }
--- src/components/component_schema.rs
+++ src/components/component_schema.rs
@@ -1,6 +1,30 @@
 use std::collections::HashMap;
 use components::component_property_schema::ComponentPropertySchema;
 
+/// Describes component action.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ComponentAction {
+    /// Type of the component action.
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub type_name: String,
+    /// Human-readable short action name.
+    pub name: String,
+    /// Human-readable action long description.
+    pub description: String,
+}
+
+/// Describes component trigger.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ComponentTrigger {
+    /// Type of the component trigger.
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub type_name: String,
+    /// Human-readable short trigger name.
+    pub name: String,
+    /// Human-readable trigger long description.
+    pub description: String,
+}
+
 /// Describes component properties and actions.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ComponentSchema {
@@ -13,4 +37,8 @@ pub struct ComponentSchema {
     pub description: String,
     /// Property type <-> property schema map.
     pub properties: HashMap<String, ComponentPropertySchema>,
+    /// Action type <-> action map.
+    pub actions: HashMap<String, ComponentAction>,
+    /// Trigger type <-> trigger map.
+    pub triggers: HashMap<String, ComponentTrigger>,
 }
