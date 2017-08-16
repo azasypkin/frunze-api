4f4445bebbb38a6313a9074c6ee33860ce153e36
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-08-16 21:33:34 +0200

    Improve RustDoc for `Exporter` and `SchematicProvider`.

--- src/schematic/fritzing/exporter.rs
+++ src/schematic/fritzing/exporter.rs
@@ -1,6 +1,5 @@
-use std;
+use std::io;
 use std::io::prelude::*;
-use std::io::Cursor;
 use std::path::PathBuf;
 
 use futures::{future, Future, Stream};
@@ -35,21 +34,35 @@ impl Exporter {
         }
     }
 
+    /// Exports Fritzing sketch into SVG file (only breadboard view is extracted right now).
+    ///
+    /// # Arguments
+    ///
+    /// * `sketch_path` - Path to the generated sketch file.
     pub fn export_sketch_to_svg(&self, sketch_path: PathBuf) -> Result<Vec<u8>> {
         // We control file name (it's UUID) so it should be a valid Unicode string.
         let file_name = sketch_path.file_name().unwrap().to_string_lossy();
-        let project_id = sketch_path.file_stem().unwrap().to_string_lossy();
+        let file_stem = sketch_path.file_stem().unwrap().to_string_lossy();
 
-        let reader = Cursor::new(self.download_export_archive(&file_name)?);
+        let downloaded_archive = self.download_export_archive(&file_name)?;
 
-        let mut zip = zip::ZipArchive::new(reader)?;
-        let breadboard_image = zip.by_name(&format!("{}_breadboard.svg", project_id))?;
+        let mut zip_archive = zip::ZipArchive::new(io::Cursor::new(downloaded_archive))?;
 
-        Ok(breadboard_image
+        let breadboard_image = zip_archive.by_name(
+            &format!("{}_breadboard.svg", file_stem),
+        )?;
+
+        breadboard_image
             .bytes()
-            .collect::<std::io::Result<Vec<u8>>>()?)
+            .collect::<io::Result<Vec<u8>>>()
+            .map_err(|e| -> Error { e.into() })
     }
 
+    /// Downloads archive with exported SVG files from the Fritzing server.
+    ///
+    /// # Arguments
+    ///
+    /// * `file_name` - name of the Fritzing sketch file to extract SVG from.
     fn download_export_archive(&self, file_name: &str) -> Result<Vec<u8>> {
         let url = Url::parse(&format!(
             "{}/svg-tcp/{}{}",
@@ -57,8 +70,6 @@ impl Exporter {
             self.schematic_base_url.as_str(),
             file_name
         ))?;
-
-        info!("URL: {}", url);
 
         let mut core = Core::new()?;
         let client = Client::new(&core.handle());
--- src/schematic/schematic_provider.rs
+++ src/schematic/schematic_provider.rs
@@ -8,11 +8,20 @@ use super::fritzing::exporter::Exporter;
 /// Manages access to the schematic related information.
 #[derive(Clone)]
 pub struct SchematicProvider {
+    /// Instance of the exporter that is responsible for converting schematic into set of SVG files.
     exporter: Exporter,
+    /// Instance of the generator that is responsible for generating schematic from the project.
     generator: Generator,
 }
 
 impl SchematicProvider {
+    /// Returns a ready schematic provider instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `schematic_base_url` - Base portion of the URL pointing to generated schematic sketches.
+    /// * `export_api_url` - URL of the Export API that will do the sketch-to-svg export.
+    /// * `generated_content_folder` - Path on the disk to store generated content to.
     pub fn new<T: Into<String>>(
         schematic_base_url: Url,
         export_api_url: Url,
@@ -25,10 +34,14 @@ impl SchematicProvider {
     }
 
     /// Gets schematic (byte array for the schematic image) for the project.
+    ///
+    /// # Arguments
+    ///
+    /// * `project` - Project to get schematic for.
     pub fn get(&self, project: Project) -> Result<Vec<u8>> {
         let path = self.generator.generate_sketch(project)?;
 
-        info!("Sketch generated: {:?}", path);
+        info!("Sketch generated and stored at {:?}", path);
 
         self.exporter.export_sketch_to_svg(path)
     }
